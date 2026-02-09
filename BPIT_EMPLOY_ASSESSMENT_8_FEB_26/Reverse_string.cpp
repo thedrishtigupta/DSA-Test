0037fbcc36937f7685fb4c605f187dbcf9adcb1a
Drishti Gupta <thedrishtigupta@gmail.com>
2026-02-09 20:51:56 +0530

    Added questions: Remove duplicates and reverse string

---
+++ BPIT_EMPLOY_ASSESSMENT_8_FEB_26/Reverse_string.cpp
@@ -0,0 +1,28 @@
+/*
+Given a string s, reverse it in place.
+
+Example:
+str = "railway"
+ans = "yawliar"
+
+Example:
+str = "dad"
+ans = "dad"
+*/
+
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+void reverseStr(string& str) {
+    int i = 0, j = str.length()-1;
+
+    while(i < j) swap(str[i++], str[j--]);
+}
+int main() {
+    string str = "railway";
+    reverseStr(str);
+    cout<<str<<endl;
+    return 0;
+}
